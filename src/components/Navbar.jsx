010cd6c84b3819786e3e734188163f56a8530d71
subedi1680 <subedi1680@gmail.com>
2025-12-14 13:06:46 +0545

    navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,138 @@
+import expertLogo from "../assets/expert_logo.png"
+import { Search } from "lucide-react";
+import { CircleChevronRight } from "lucide-react";
+import { Menu } from "lucide-react";
+
+function Navbar(){
+    return(
+        <>
+        {/* nav bar and background image */}
+      <div className="relative bg-[url(assets/home_expert_image.png)] bg-cover bg-center h-[450px] md:h-[600px] flex flex-col backdrop-blur-3xl md:w-full z-10">
+        <nav className="flex flex-wrap justify-between border-b border-[#3A52B0] pl-10 pr-4 md:px-10">
+          <div className="my-3 -mx-6 md:mx-3">
+            <img src={expertLogo} className="h-15 "></img>
+          </div>
+          <div className="px-11 flex-1">
+            <ul className="hidden space-x-1 md:flex flex-row text-white text-sm font-bold justify-end">
+              <li className="group hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200 relative">
+                <span>ABOUT US</span>
+                <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-[#4b5c81] w-63 text-md font-medium">
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    About Expert
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Message from Director
+                  </li>
+                </ul>
+              </li>
+              <li className="group hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200 relative">
+                <span>STUDY ABROAD</span>
+                <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-[#4b5c81] w-63 text-md font-medium">
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Study in Australia
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Study in Canada
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Study in UK
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Study in USA
+                  </li>
+                </ul>
+              </li>
+              <li className="group hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200 relative">
+                <span>STUDENT SERVICES</span>
+                <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-[#4b5c81] w-63 text-md font-medium">
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Visa Application
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    PTE Test Preparation
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    IELTS Training
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Study Abroad Accomodation
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Student Health Insurance
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Banking in Australia
+                  </li>
+                </ul>
+              </li>
+              <li className="group hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200 relative">
+                <span>VISA</span>
+                <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-[#4b5c81] w-63 text-md font-medium">
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Visitor Visa
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Parent Visa
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Partner Visa
+                  </li>
+                </ul>
+              </li>
+              <li className="hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200">
+                EVENTS
+              </li>
+              <li className="hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200">
+                BLOG
+              </li>
+              <li className="group hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200 relative">
+                <span>CONTACT US</span>
+                <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-[#4b5c81] w-63 text-md font-medium">
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Find nearest office
+                  </li>
+                  <li className="px-4 py-4 hover:bg-[#2165D6] hover:text-white cursor-pointer border-b border-gray-200">
+                    Other Websites
+                  </li>
+                </ul>
+              </li>
+              <li className="hover:bg-[#186FE9] cursor-pointer px-2.75 py-8 duration-200">
+                NPL PREDICT & WIN
+              </li>
+            </ul>
+          </div>
+          <div className="md:flex hidden items-center border-l border-[#3A52B0] pl-10 pr-1">
+            <Search color="white" />
+          </div>
+          <div className="flex md:hidden items-center">
+            <Menu color="white" strokeWidth="2" size="30" />
+          </div>
+        </nav>
+        <div className="flex flex-col mt-20 md:mt-12 ml-7 md:ml-30 text-white">
+          <div className="underline underline-offset-8  md:underline-offset-10 decoration-1 font-extrabold text-sm md:tracking-[.5em] tracking-[.25em]">
+            EXPERT EDUCATION
+          </div>
+          <div className="text-4xl md:text-5xl font-bold mt-3 md:mt-9 tracking-[0.06em] md:tracking-[0em]">
+            Explore your
+          </div>
+          <div className="text-4xl md:text-5xl font-bold md:mt-2 tracking-[0.07em] md:tracking-[0em]">
+            Dreams Overseas
+          </div>
+          <div className="flex flex-row justify-start items-center text-md mt-3 space-x-1">
+            <div className="hidden md:flex">
+              <CircleChevronRight strokeWidth="1" size="40" />
+            </div>
+            <div className="flex md:hidden">
+              <CircleChevronRight strokeWidth="1" size="25" />
+            </div>
+            <div className="font-semibold md:tracking-widest text-[12px] md:text-[16px]">
+              Discover your options
+            </div>
+          </div>
+        </div>
+      </div>
+        </>
+    );
+};   
+
+export default Navbar;
